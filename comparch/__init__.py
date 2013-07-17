9ffa7fc79eb6e0d44d1ae24ed5f67142f9a0b680
faassen <faassen@startifact.com>
2013-07-17 22:21:59 +0200

    Expose a few more things to the public.

--- comparch/__init__.py
+++ comparch/__init__.py
@@ -2,4 +2,5 @@ from .implicit import implicit
 from .registry import ClassRegistry, Registry
 from .lookup import Lookup, CachedLookup
 from .interface import Interface
-
+from .predicate import PredicateRegistry
+from .compose import ListClassLookup, ChainClassLookup, CachedClassLookup
