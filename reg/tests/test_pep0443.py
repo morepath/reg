93a216438f2f1cbede8a4b7f2315e40a63bb4416
faassen <faassen@startifact.com>
2013-09-24 23:24:50 +0200

    Build pep443 on top of Reg's map facility.

---
+++ reg/tests/test_pep0443.py
@@ -0,0 +1,66 @@
+from reg.dispatch import singledispatch
+from decimal import Decimal
+
+@singledispatch
+def fun(arg, verbose=False):
+    if verbose:
+        return "Let me just say, %s" % arg
+    return arg
+
+
+@fun.register(int)
+def _(arg, verbose=False):
+    if verbose:
+        return "Strength in numbers, eh? %s" % arg
+    return arg
+
+
+@fun.register(list)
+def _(arg, verbose=False):
+    result = []
+    if verbose:
+        result.append("Enumerate this:\n")
+    for i, elem in enumerate(arg):
+        result.append("%s %s\n" % (i, elem))
+    return ''.join(result)
+
+
+def nothing(arg, verbose=False):
+    return "Nothing."
+
+
+@fun.register(float)
+@fun.register(Decimal)
+def fun_num(arg, verbose=False):
+    if verbose:
+        return "Half of your number: %s" % arg
+    return arg / 2
+
+
+def test_string():
+    assert fun("Hello, world.") == "Hello, world."
+    assert fun("test.", verbose=True) == "Let me just say, test."
+
+
+def test_int():
+    assert fun(42, verbose=True) == "Strength in numbers, eh? 42"
+
+
+def test_list():
+    assert fun(['spam', 'spam', 'eggs', 'spam'], verbose=True) == '''\
+Enumerate this:
+0 spam
+1 spam
+2 eggs
+3 spam
+'''
+
+
+def test_none():
+    fun.register(type(None), nothing)
+    assert fun(None) == "Nothing."
+
+
+def test_multi_reg():
+    assert fun(1.23) == 0.615
+    assert fun(Decimal('1.23')) == Decimal('0.615')
