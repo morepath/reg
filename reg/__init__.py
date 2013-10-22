28316131afacbbc8fa39ff7242c0ecc7bb883792
faassen <faassen@startifact.com>
2013-10-22 21:08:38 +0200

    Export PredicateRegistryError.

--- reg/__init__.py
+++ reg/__init__.py
@@ -3,5 +3,6 @@ from .implicit import implicit
 from .registry import ClassRegistry, Registry
 from .lookup import Lookup, LookupError
 from .interface import Interface, abstractmethod, abstractproperty
-from .predicate import PredicateRegistry, Predicate, KeyIndex
+from .predicate import (PredicateRegistry, Predicate, KeyIndex,
+                        PredicateRegistryError)
 from .compose import ListClassLookup, ChainClassLookup, CachedClassLookup
