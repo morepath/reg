9e01211938b9f31e15b9b83d2a39c4980951e9dc
faassen <faassen@startifact.com>
2013-10-22 21:11:18 +0200

    Integrate implicit docstrings into implicit module.

--- reg/implicit.py
+++ reg/implicit.py
@@ -3,35 +3,93 @@ explicitly.
 """
 
 import threading
-from .interfaces import IImplicit
 
 
-class Implicit(IImplicit):
+class Implicit(object):
+    """Implicit global lookup.
+
+    There will only one singleton instance of this, called ``implicit``.
+
+    Normally during startup of an application the framework will
+    register the implicit lookup by using ``implicit.register()``.
+
+    The lookup can then be accessed using ``implicit.lookup``.
+
+    ``Interface.component()`` and ``Interface.adapt`` make use of this
+    information if you do not pass an explicit ``lookup`` keyword
+    argument. This is handy as it becomes unnecessary to have to pass
+    a ``lookup`` object everywhere.
+
+    The drawback is that this single global lookup is implicit, which
+    makes it harder to test in isolation. Reg supports testing
+    with the explicit ``lookup`` argument, but that is not useful if
+    you are testing code that relies on an implicit lookup. Therefore
+    Reg has strived to make the implicit global lookup as
+    explicit as possible so that it can be manipulated in tests where
+    this is necessary.
+
+    It is also possible for a framework to change the implicit lookup
+    during run-time. This is done by simply assigning to
+    ``implicit.lookup``. The lookup is stored on a thread-local and is
+    unique per thread.
+
+    Reg offers facilities to compose such a custom lookup:
+
+    * ``reg.ListClassLookup`` and ``reg.ChainClassLookup``
+       which can be used to chain multiple ``IClassLookup``s together.
+
+   * ``reg.CachedClassLookup`` which can be used to create a
+      faster caching version of an ``IClassLookup``.
+
+    * ``reg.Lookup`` which can be used to turn a ``IClassLookup``
+      into a proper ``ILookup``.
+
+    To change the lookup back to a lookup in the global implicit
+    registry, call ``reset_lookup``.
+
+    The implicit lookup is thread-local: each thread has a separate
+    implicit global lookup.
+    """
+
     def __init__(self):
         self.base_lookup = None
         self.local = None
 
     def initialize(self, lookup):
+        """Initialize implicit with lookup.
+        """
+
         self.base_lookup = lookup
         self.local = Local(lookup=self.base_lookup)
 
     def clear(self):
+        """Clear global implicit lookup.
+        """
         self.base_lookup = None
         self.local = Local(lookup=None)
 
     def reset(self):
+        """Reset global implicit lookup to original lookup used for
+        registration.
+
+        This can be used to wipe out any composed lookups that
+        were installed during this thread.
+        """
         self.local.lookup = self.base_lookup
 
     @property
     def lookup(self):
+        """Get the implicit ILookup."""
         if self.local is None:
             return None
         return self.local.lookup
 
     @lookup.setter
     def lookup(self, value):
+        """Set the implicit ILookup."""
         self.local.lookup = value
 
+    # XXX also document base_lookup
 
 class Local(threading.local):
     def __init__(self, **kw):
