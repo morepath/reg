cad5e576058a6d73dd1acd649e5a0486887b2b89
faassen <faassen@startifact.com>
2013-12-03 21:54:49 +0100

    A few more tests. All is okay, but we still are hunting an error.

--- reg/tests/test_mapply.py
+++ reg/tests/test_mapply.py
@@ -97,3 +97,64 @@ def test_mapply_all_args_kw():
     assert mapply(foo, 2, b=1) == (2, (), {'b': 1})
     assert mapply(foo, 2, 3, b=1) == (2, (3,), {'b': 1})
 
+
+def test_mapply_class():
+    class Foo(object):
+        def __init__(self):
+            pass
+    assert isinstance(mapply(Foo), Foo)
+
+
+def test_mapply_class_too_much():
+    class Foo(object):
+        def __init__(self):
+            pass
+    assert isinstance(mapply(Foo, a=1), Foo)
+
+
+def test_mapply_class_no_init_too_much():
+    class Foo(object):
+        pass
+    variables = {'base': None }
+    assert isinstance(mapply(Foo, **variables), Foo)
+
+
+def test_mapply_kw_class():
+    class Foo(object):
+        def __init__(self, **kw):
+            self.kw = kw
+    assert mapply(Foo, a=1).kw == {'a': 1}
+
+
+def test_mapply_args_class():
+    class Foo(object):
+        def __init__(self, *args):
+            self.args = args
+    assert mapply(Foo, a=1).args == ()
+    assert mapply(Foo, 1).args == (1,)
+
+
+def test_mapply_args_kw_class():
+    class Foo(object):
+        def __init__(self, *args, **kw):
+            self.args = args
+            self.kw = kw
+    r = mapply(Foo, a=1)
+    assert (r.args, r.kw) == ((), {'a': 1})
+    r = mapply(Foo, 1)
+    assert (r.args, r.kw) == ((1,), {})
+    r = mapply(Foo, 1, a=1)
+    assert (r.args, r.kw) == ((1,), {'a': 1})
+
+def test_mapply_all_args_kw_class():
+    class Foo(object):
+        def __init__(self, a, *args, **kw):
+            self.a = a
+            self.args = args
+            self.kw = kw
+    r = mapply(Foo, 1)
+    assert (r.a, r.args, r.kw) == (1, (), {})
+    r = mapply(Foo, 2, b=1)
+    assert (r.a, r.args, r.kw) == (2, (), {'b': 1})
+    r = mapply(Foo, 2, 3, b=1)
+    assert (r.a, r.args, r.kw) == (2, (3,), {'b': 1})
