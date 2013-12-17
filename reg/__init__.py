e7045e1a2a328b705732a10b1a7fede962d30629
faassen <faassen@startifact.com>
2013-12-17 12:27:26 +0100

    Expose PredicateMatcher.

--- reg/__init__.py
+++ reg/__init__.py
@@ -3,7 +3,7 @@ from .implicit import implicit, NoImplicitLookupError
 from .registry import ClassRegistry, Registry, IRegistry, IClassLookup
 from .lookup import Lookup, ComponentLookupError, Matcher
 from .predicate import (PredicateRegistry, Predicate, KeyIndex,
-                        PredicateRegistryError)
+                        PredicateRegistryError, PredicateMatcher)
 from .compose import ListClassLookup, ChainClassLookup, CachingClassLookup
 from .generic import generic
 from .mapply import mapply
