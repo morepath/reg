d4c985c1cd07a59eb312130f1d4317ab12892570
faassen <faassen@startifact.com>
2014-01-21 13:28:43 +0100

    Make ANY public.

--- reg/__init__.py
+++ reg/__init__.py
@@ -4,7 +4,7 @@ from .implicit import implicit, NoImplicitLookupError
 from .registry import ClassRegistry, Registry, IRegistry, IClassLookup
 from .lookup import Lookup, ComponentLookupError, Matcher
 from .predicate import (PredicateRegistry, Predicate, KeyIndex,
-                        PredicateRegistryError, PredicateMatcher)
+                        PredicateRegistryError, PredicateMatcher, ANY)
 from .compose import ListClassLookup, ChainClassLookup, CachingClassLookup
 from .generic import generic
 from .mapply import mapply, arginfo
