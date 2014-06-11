f99d30c0023f771852db815d50cb0f655cf62656
faassen <faassen@startifact.com>
2014-06-11 14:22:48 +0200

    Next version is 0.7.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ tests_require = [
     ]
 
 setup(name='reg',
-      version='0.6.1.dev0',
+      version='0.7.dev0',
       description="Generic functions. Clever registries and lookups",
       long_description=long_description,
       author="Martijn Faassen",
