8e2448ff892565666369809324971ab6408933df
faassen <faassen@startifact.com>
2014-06-17 13:52:56 +0200

    Back to development: 0.8

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ tests_require = [
     ]
 
 setup(name='reg',
-      version='0.7',
+      version='0.8.dev0',
       description="Generic functions. Clever registries and lookups",
       long_description=long_description,
       author="Martijn Faassen",
