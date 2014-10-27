2203c1864fa09d0f12b7d9982ac74274d4df219d
faassen <faassen@startifact.com>
2014-10-27 16:06:04 +0100

    No more compose module.

--- reg/__init__.py
+++ reg/__init__.py
@@ -5,7 +5,6 @@ from .registry import ClassRegistry, Registry, IRegistry, IClassLookup
 from .lookup import Lookup, ComponentLookupError, Matcher
 from .predicate import (PredicateRegistry, Predicate, KeyIndex, ClassIndex,
                         PredicateRegistryError, PredicateMatcher, ANY)
-from .compose import ListClassLookup, ChainClassLookup, CachingClassLookup
 from .generic import generic, classgeneric, dispatch
 from .mapply import mapply
 from .arginfo import arginfo
