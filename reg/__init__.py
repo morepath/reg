af2b65e5a771ad945bcb25213fe4232730fa45e8
faassen <faassen@startifact.com>
2014-10-28 11:54:45 +0100

    Expose indexes.

--- reg/__init__.py
+++ reg/__init__.py
@@ -6,7 +6,7 @@ from .mapply import mapply
 from .arginfo import arginfo
 from .sentinel import Sentinel, NOT_FOUND
 from .error import RegistrationError, KeyExtractorError, NoImplicitLookupError
-from .predicate import (Predicate, PredicateRegistry,
+from .predicate import (Predicate, PredicateRegistry, KeyIndex, ClassIndex,
                         key_predicate, class_predicate,
                         match_key, match_instance, match_argname,
                         match_class)
