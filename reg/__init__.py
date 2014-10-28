bf84fe6462eed5522627ad50bc728713f2ab1cdb
faassen <faassen@startifact.com>
2014-10-28 12:44:03 +0100

    Add KeyExtractor to exports.

--- reg/__init__.py
+++ reg/__init__.py
@@ -4,6 +4,7 @@ from .registry import Registry, CachingKeyLookup, Lookup
 from .dispatch import dispatch
 from .mapply import mapply
 from .arginfo import arginfo
+from .argextract import KeyExtractor
 from .sentinel import Sentinel, NOT_FOUND
 from .error import RegistrationError, KeyExtractorError, NoImplicitLookupError
 from .predicate import (Predicate, PredicateRegistry, KeyIndex, ClassIndex,
