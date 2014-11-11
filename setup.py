c54936e9368eb8ad28296d1b206021916e0a0e82
faassen <faassen@startifact.com>
2014-11-11 21:49:51 +0100

    Preparing release 0.9.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ tests_require = [
     ]
 
 setup(name='reg',
-      version='0.9.1.dev0',
+      version='0.9.1',
       description="Generic functions. Clever registries and lookups",
       long_description=long_description,
       author="Martijn Faassen",
