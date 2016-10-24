6b84d06b4c0c9c49b931539d2db839054e529c5f
taschini <taschini@users.noreply.github.com>
2016-10-24 23:15:35 +0200

    Removed PredicateRegistry from the API.

--- reg/__init__.py
+++ reg/__init__.py
@@ -4,6 +4,6 @@ from .context import (dispatch_method, DispatchMethod,
                       methodify, clean_dispatch_methods)
 from .arginfo import arginfo
 from .error import RegistrationError
-from .predicate import (Predicate, PredicateRegistry, KeyIndex, ClassIndex,
+from .predicate import (Predicate, KeyIndex, ClassIndex,
                         match_key, match_instance, match_class)
 from .cache import DictCachingKeyLookup, LruCachingKeyLookup
