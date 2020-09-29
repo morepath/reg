9cc42179f38da4c3f7b5ba52c5b226465c23814d
Samuel Gaist <samuel.gaist@kde.org>
2020-09-29 23:09:52 +0200

    [setup] Update list of supported Python versions

--- setup.py
+++ setup.py
@@ -24,8 +24,6 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
